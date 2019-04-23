7efa8c61b3b40bd53b001e2f73be09b47fe6cadd
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-04-23 17:15:09 -0500

    Implement basic list notes functionality.
    Was able to get the list notes command working and printing a nice looking list.
    Should probably have the list notes command fire whenever notes are added or deleted later on.
    Still need to add some color to this menu and maybe some better formatting.

--- utils.js
+++ utils.js
@@ -15,6 +15,7 @@ const fs = require('fs')
 const log = require('./log')
 
 const dataStorePath = './notes.json'
+const l = console.log // laziness method...
 
 const create = (title, body) => {
   // load the notes from the filesystem
@@ -34,8 +35,21 @@ const create = (title, body) => {
   }
 }
 
+/*
+Current Notes:
+
+-- Title --    ||  -- Body --
+Allie Presents ||  Trip To Port Arthur, Spa Day
+Dog Stuff      ||  Vet Appointment, Flea Medicine
+Grocery List   ||  Eggs, Ham, Cheese, Bread (this could be colored)
+*/
 const list = () => {
-  log.logMsg('success', 'List function working!')
+  const notes = loadNotes()
+  if (notes.length > 0) {
+    listNotes(notes)
+  } else {
+    log.logMsg('error', 'There are no notes to list!')
+  }
 }
 
 
@@ -66,6 +80,48 @@ const saveNotes = (notes) => {
 const isUniqueTitle = (notes, title) => {
   // if the title exists in the notes array, notes.some() will return true, meaning we have a duplicate title. This method's name leads us to return true if there is NO duplicate title, so we need to reverse the logic.
   return !(notes.some(note => note.title === title))
+}
+
+const listNotes = (notes) => {
+  const menu = formatMenu(notes)
+  l(menu.header)
+  menu.body.forEach(note => l(note))
+}
+
+const formatMenu = (notes) => {
+  
+  const titleColumnWidth = getTitleColumnWidth(notes)
+  const centerColumn = ' || '
+  const menu = {}
+
+  menu.header = formatTitleColumn('-- Title --', titleColumnWidth) + centerColumn + '-- Body --'
+  
+  menu.body = notes.map(note => formatTitleColumn(note.title, titleColumnWidth) + centerColumn + note.body)
+
+  return menu 
+} 
+
+const formatTitleColumn = (title, titleColumnWidth) => {
+  const numExtraSpaces = titleColumnWidth - title.length
+  for (let i = 0; i < numExtraSpaces; i++) {
+    title += ' '
+  }
+  return title
+}
+
+const getTitleColumnWidth = (notes) => {
+
+  // title column will have:
+  // -- Title --
+  // which needs 11 spaces
+  const MIN_COL_WIDTH = 11
+  
+  // we need to figure out the longest note title so we can set the width of the menu, this is the callback function that will do that when passed to Array.reduce()
+  const reducer = (longestNoteTitle, { title }) => title.length > longestNoteTitle ? title.length : longestNoteTitle 
+
+  const longestNoteTitle = notes.reduce(reducer, 0)
+
+  return MIN_COL_WIDTH >= longestNoteTitle ? MIN_COL_WIDTH : longestNoteTitle
 }
 
 module.exports = {
